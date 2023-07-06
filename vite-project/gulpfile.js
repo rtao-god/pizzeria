ca1885039db14dda83c948ee93277deac4704c20
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-07-06 21:59:28 +0300

    jsx

--- vite-project/gulpfile.js
+++ vite-project/gulpfile.js
@@ -2,7 +2,6 @@ const {src, dest} = require('gulp')
 
 const sass = require("gulp-sass")(require("sass"))
 
-
 function styles() {
     return src('src/mainSass/main.sass')
         .pipe(sass())
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './Styles/index.css'
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
